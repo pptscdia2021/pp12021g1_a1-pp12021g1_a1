ba5425b562b4ab290beeeeb13422f16730b10ce0
teconchorizo <87441391+teconchorizo@users.noreply.github.com>
2021-09-13 11:02:13 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -1,7 +1,14 @@
-def suma(a,b):
-    return(a+b)
+def suma(a, b):
+    return (a + b)
+
+
+def resta(c, d):
+    return (c - d)
+
 
 if __name__ == "__main__":
     print("Principal")
     print()
-    print("La suma es: ", str(suma(2,3)))
+    print("La suma es: ", str(suma(2, 3)))
+    print(f"La resta es: {int(resta(5, 6))}")
+
