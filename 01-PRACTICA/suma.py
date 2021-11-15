db81ba06f9e3916cd31a08fba89502f7d903e2f5
AbssCnn <85201822+AbssCnn@users.noreply.github.com>
2021-11-15 15:23:35 -0300

    Create suma.py

---
+++ 01-PRACTICA/suma.py
@@ -0,0 +1,9 @@
+MONICA LO HIZO, ABIGAIL LO MOVIO A ESTA CARPETA
+
+def suma(a,b):
+    return(a+b)
+
+if __name__ == "__main__":
+    print("Principal")
+    print()
+    print("La suma es: ", str(suma(2,3)))
