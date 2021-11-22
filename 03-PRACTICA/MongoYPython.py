1d7478f89d6f8d75ad4d5e2e524434a29f8257e6
AbssCnn <85201822+AbssCnn@users.noreply.github.com>
2021-11-22 15:47:39 -0300

    Create MongoYPython.py

---
+++ 03-PRACTICA/MongoYPython.py
@@ -0,0 +1,22 @@
+import pymongo
+import pandas as pd
+
+#Conectamos a mongo
+client = pymongo.MongoClient('localhost',27017)
+db = client['mongo']
+
+collection = db['income']
+
+with open('./tercera_practica/db_mongo/weatherHistory.csv', newline='') as finput:
+    column_names = finput.readline()
+    column_names_list = column_names.split(',')
+
+    for line in finput:
+        row_list =line.rstrip('\n').split(',')
+        row_dict = dict(zip(column_names_list,row_list))
+        try:
+            collection.insert_one(row_dict)
+        except:
+            pass
+
+print('Se insertaron: ', collection.count(), 'registros a la coleccion')
