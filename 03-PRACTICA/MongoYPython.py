3bc2e8002bb87eb752f534967687de17381b676c
AbssCnn <85201822+AbssCnn@users.noreply.github.com>
2021-11-22 15:50:50 -0300

    Update MongoYPython.py

--- 03-PRACTICA/MongoYPython.py
+++ 03-PRACTICA/MongoYPython.py
@@ -1,13 +1,15 @@
-'Llamamos a las librerias'
+'Llamamos a las librerias.'
 import pymongo
 import pandas as pd
 
-'Conectamos a mongo'
+'Conectamos a Mongo.'
 client = pymongo.MongoClient('localhost',27017)
 db = client['mongo']
 
+'Coleccion.'
 collection = db['income']
 
+'Abrir con el archivo "xx".'
 with open('./03-PRACTICA/SQL practica/weatherHistory.csv', newline='') as finput:
     column_names = finput.readline()
     column_names_list = column_names.split(',')
